ecd99bebab72a99998b865379ab19b315ef4602e
oueue <39622401+oueue@users.noreply.github.com>
2019-06-11 10:45:21 +0800

    Create 1.cpp
    project1\header.h(41): error C3646: 'salesmen': unknown override specifier
    project1\header.h(41): error C4430: missing type specifier - int assumed. Note: C++ does not support default-int
    project1\header.h(45): error C2061: syntax error: identifier 'PTSalesman'
    project1\header.h(46): error C2061: syntax error: identifier 'PTSalesman'
    project1\1.cpp(66): warning C4244: 'return': conversion from 'double' to 'float', possible loss of data
    project1\1.cpp(119): error C2065: 'salesmen': undeclared identifier
    project1\1.cpp(121): error C2065: 'salesmen': undeclared identifier
    project1\1.cpp(123): warning C4244: '=': conversion from 'double' to 'float', possible loss of data
    project1\1.cpp(124): warning C4244: 'return': conversion from 'double' to 'float', possible loss of data
    project1\1.cpp(126): error C2614: 'SaleManager': illegal member initialization: 'salesmen' is not a base or member
    project1\1.cpp(133): error C2511: 'void SaleManager::addSalesmen(PTSalesman *)': overloaded member function not found in 'SaleManager'
    project1\header.h(34): note: see declaration of 'SaleManager'
    project1\1.cpp(134): error C2065: 'salesmen': undeclared identifier
    project1\1.cpp(138): error C2511: 'void SaleManager::deleteSalesmen(PTSalesman *)': overloaded member function not found in 'SaleManager'
    project1\header.h(34): note: see declaration of 'SaleManager'
    project1\1.cpp(141): error C2065: 'salesmen': undeclared identifier
    project1\1.cpp(145): error C2065: 'salesmen': undeclared identifier
    project1\1.cpp(190): error C2660: 'SaleManager::addSalesmen': function does not take 1 arguments
    project1\header.h(45): note: see declaration of 'SaleManager::addSalesmen'
    project1\1.cpp(191): error C2660: 'SaleManager::addSalesmen': function does not take 1 arguments
    project1\header.h(45): note: see declaration of 'SaleManager::addSalesmen'

---
+++ 1.cpp
@@ -0,0 +1,197 @@
+/*file "1.cpp*/
+#include <iostream>
+#include "Header.h"
+using namespace std;
+
+
+//employee methods implementation
+employee* employees[10000];
+int employee::maxNum = 0;
+
+void employee::setNum(int Num)
+{
+	this->Num = Num;
+}
+int employee::getNum()
+{
+	return Num;
+}
+int employee::calLevel(float sal)
+{
+	if (sal == 100)
+	{
+		Level = 1;
+		return 1;
+	}
+	if (sal == 200) 
+	{
+		Level = 2;
+		return 2;
+	}
+	else
+	{
+		Level = 0;
+		return 0;
+	}
+}
+int employee::getLevel()
+{
+	return Level;
+}
+void employee::setSalary(double sal)
+{
+	Salary = (float)sal;
+}
+float employee::getSalary()
+{
+	return Salary;
+}
+void employee::setName(const char* name)
+{
+	Name = name;
+}
+const char*  employee::getName()
+{
+	return Name;
+}
+int employee::getMaxNum()
+{
+	return maxNum;
+}
+
+
+//PTSalesman methods implementation
+float PTSalesman::getSalary()
+{
+	return Sale * 0.04;
+}
+
+float PTSalesman::getSale()
+{
+	return Sale;
+}
+PTSalesman::PTSalesman(const char* name,  float Sale) 
+{
+	setName(name);
+	this->Sale = Sale;
+	setSalary(getSalary());
+	calLevel(getSalary());
+	employees[maxNum] = this;
+	maxNum++;
+}
+PTSalesman::PTSalesman()
+{
+	employees[maxNum] = this;
+	maxNum++;
+}
+PTSalesman::~PTSalesman()
+{
+	maxNum--;
+}
+float PTTechnician :: getSalary()
+{
+	setSalary(WorkHour * 100.0);
+	return getSalary();
+}
+
+
+//PTTechnician methods implemetation
+PTTechnician::PTTechnician(const char* name, int level, int WorkHour)
+{
+	this->WorkHour = WorkHour;
+	setName(name);
+	setSalary(getSalary());
+	calLevel(getSalary());
+	employees[maxNum] = this;
+	maxNum++;
+}
+PTTechnician::~PTTechnician()
+{
+	maxNum--;
+}
+
+
+//SaleManager methods implementation
+int SaleManager::num = 0;
+float SaleManager::getSalary()
+{
+	double s = 0;
+	for (int i = 0; i <salesmen.getsize(); ++i)
+	{
+		s = s + salesmen[i].getSale();
+	}
+	departmentSale = s;
+	return fixedSal + 0.05*s;
+}
+SaleManager::SaleManager(const char* name, float fixedSalary, int sz = 0):salesmen(sz)
+{
+	setName(name);
+	fixedSal = fixedSalary;
+	setSalary(fixedSalary + getSalary());
+}
+SaleManager::~SaleManager()
+{
+	maxNum--;
+}
+void SaleManager::addSalesmen(PTSalesman* s)
+{
+	salesmen[num] = *s;
+	num++;
+}
+void SaleManager::deleteSalesmen(PTSalesman* s)
+{
+	for (int i = 0; i < num; i++)
+	{
+		if (salesmen[i].getNum() == s->getNum())
+		{
+			for (int j = i; j < num-1; j++)
+			{
+				salesmen[j] = salesmen[j + 1];
+			}
+			num--;
+			return;
+		}
+	}
+}
+
+//Manager methods implementation
+Manager::Manager()
+{
+	setSalary(8000);
+	employees[maxNum] = this;
+	maxNum++;
+}
+Manager::~Manager()
+{
+	maxNum--;
+}
+
+
+//Array Implementations
+Array::Array(int sz)
+{
+	size = sz;
+	list = new PTSalesman[sz];
+}
+Array::~Array()
+{
+	delete[]list;
+}
+int Array::getsize()
+{
+	return size;
+}
+PTSalesman& Array::operator[](int i)
+{
+	return list[i];
+}
+int main()
+{
+	PTSalesman a("x", 30000);
+	PTSalesman b("y", 3000);
+	PTSalesman c("z", 430000);
+	SaleManager x("j", 30000, 4);
+	x.addSalesmen(&a);
+	x.addSalesmen(&b);
+	cout << x.getSalary() << endl;
+}
